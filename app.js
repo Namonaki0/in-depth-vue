bb2e7d513b7e3093b67ec670d73651976f52f7ca
Andre <andreferreira@live.co.uk>
2022-09-17 23:49:20 +0100

    more study material

--- app.js
+++ app.js
@@ -7,6 +7,32 @@ const app = Vue.createApp({
       country: "America",
       x: 0,
       y: 0,
+      albums: [
+        {
+          artist: "Palisades",
+          desc: "Reaching Hypercritical",
+          albumCover: "assets/img1.jpg",
+          isFav: true,
+        },
+        {
+          artist: "Dir En Grey",
+          desc: "Phalaris",
+          albumCover: "assets/img2.jpg",
+          isFav: false,
+        },
+        {
+          artist: "Slipknot",
+          desc: "The End So Far",
+          albumCover: "assets/img3.jpg",
+          isFav: false,
+        },
+        {
+          artist: "The Gazette",
+          desc: "MASS",
+          albumCover: "assets/img4.jpg",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
@@ -20,14 +46,18 @@ const app = Vue.createApp({
       this.isShowing = !this.isShowing;
     },
     handleEvent(e, num) {
-      console.log("event", e, e.type);
+      // console.log("event", e, e.type);
       if (num) {
-        console.log(num);
+        // console.log(num);
       }
     },
     handleMousemove(e) {
       this.x = e.offsetX;
       this.y = e.offsetY;
+    },
+    toggleFav(album) {
+      album.isFav = !album.isFav;
+      console.log(album.isFav);
     },
   },
 });
--- notes.js
+++ notes.js
@@ -1,4 +1,4 @@
-// DIRECTIVES
+// directives
 // v-on - shorthand: @ --> i.e: @click=""
 
 // conditional rendering
@@ -6,3 +6,10 @@
 // v-show - CSS based --> hides the element - better performance for elements that change often
 
 // methods inherit the event object automatically
+
+// attribute / data binding
+// v-bind:  --> shorthand: : --> i.e: :href=""
+
+// computed properties
+// a way to define a data property inside the component
+// it depends on other data - when data changes the computed property will update
